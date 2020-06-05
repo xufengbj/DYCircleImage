e88a57aeec2ca5850bc9bed0f484bc57a2e22681
xufeng <1192936115@qq.com>
2020-06-05 09:11:53 +0800

    添加类库

--- Example/DYCircleImage/DYViewController.m
+++ Example/DYCircleImage/DYViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "DYViewController.h"
+#import <ZYBaseUtilsFramework/ZYLogTool.h>
 
 @interface DYViewController ()
 
@@ -18,6 +19,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [ZYLogTool printObjcName:self];
 }
 
 - (void)didReceiveMemoryWarning
